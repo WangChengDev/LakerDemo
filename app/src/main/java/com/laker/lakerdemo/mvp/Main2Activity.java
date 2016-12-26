8e2d989389f1dd0b17454ea75d22efd8e27ca21c
wangcheng <wangcheng@yuantuan.com>
2016-12-26 10:50:40 +0800

    creat mvp files

--- app/src/main/java/com/laker/lakerdemo/mvp/Main2Activity.java
+++ app/src/main/java/com/laker/lakerdemo/mvp/Main2Activity.java
@@ -1,35 +1,78 @@
 package com.laker.lakerdemo.mvp;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import com.laker.frame.util.show.L;
 import com.laker.frame.util.show.T;
 import com.laker.lakerdemo.R;
 import com.laker.lakerdemo.base.activity.MyBaseCommonActivity;
+import com.laker.lakerdemo.mvp.bean.User;
+import com.laker.lakerdemo.mvp.contract.LoginContract;
+import com.laker.lakerdemo.mvp.presenter.LoginPresenterImpl;
+import com.laker.lakerdemo.utils.CoordinateTransformUtil;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class Main2Activity extends MyBaseCommonActivity {
+public class Main2Activity extends MyBaseCommonActivity implements LoginContract.View {
 
-    @Bind(R.id.button)
-    Button mButton;
-    @Bind(R.id.button2)
-    Button mButton2;
-    @Bind(R.id.button3)
-    Button mButton3;
-    @Bind(R.id.activity_main2)
-    RelativeLayout mActivityMain2;
 
+    @Bind(R.id.et_name)
+    EditText mEtName;
+    @Bind(R.id.et_pwd)
+    EditText mEtPwd;
+    @Bind(R.id.btn_login)
+    Button mBtnLogin;
+    @Bind(R.id.btn_cancle)
+    Button mBtnCancle;
+    @Bind(R.id.label)
+    TextView mLabel;
+
+    //    private UserLoginPresenterImpl loginPresenter;
+    private LoginPresenterImpl loginPresenter2;
+
+    @TargetApi(Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        loginPresenter = new UserLoginPresenterImpl(this);
+        loginPresenter2 = new LoginPresenterImpl(this);
 
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        }else {
+            startSysLBS();
+        }
+
+
+    }
+
+    public void startSysLBS() {
+        String serviceString = Context.LOCATION_SERVICE;
+        String provider = LocationManager.GPS_PROVIDER;
+        LocationManager locationManager = (LocationManager) getSystemService(serviceString);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+        getLocationInfo(location);
+        locationManager.requestLocationUpdates(provider, 2000, 0, locationListener);
     }
 
     @Override
@@ -42,24 +85,99 @@ public class Main2Activity extends MyBaseCommonActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick({R.id.button, R.id.button2, R.id.button3})
+
+    @OnClick({R.id.btn_login, R.id.btn_cancle})
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.button:
-                T.showShort(this,"button");
-                L.d("button");
-                Log.d("11111111111","111111111111button");
+            case R.id.btn_login:
+                loginPresenter2.login();
                 break;
-            case R.id.button2:
-                T.showShort(this,"button2");
-                L.d("button2");
-                Log.d("11111111111","111111111111button2");
-                break;
-            case R.id.button3:
-                T.showShort(this,"button3");
-                L.d("button3");
-                Log.d("11111111111","111111111111button3");
+            case R.id.btn_cancle:
+                loginPresenter2.clear();
                 break;
         }
     }
+
+    @Override
+    public String getUserName() {
+        return mEtName.getText().toString();
+    }
+
+    @Override
+    public String getPassword() {
+        return mEtPwd.getText().toString();
+    }
+
+    @Override
+    public void clearUserName() {
+
+        mEtName.getText().clear();
+    }
+
+    @Override
+    public void clearPassword() {
+        mEtPwd.getText().clear();
+    }
+
+    @Override
+    public void showLoading() {
+        T.showShort(this, "showLoading");
+    }
+
+    @Override
+    public void hideLoading() {
+        T.showShort(this, "hideLoading");
+    }
+
+    @Override
+    public void toNextActivity(User user) {
+        T.showShort(this, "toNextActivity");
+    }
+
+    @Override
+    public void showFailedError() {
+        T.showShort(this, "showFailedError");
+    }
+
+    private void getLocationInfo(Location location) {
+        String latLongInfo;
+        if (location != null) {
+            double latitude = location.getLatitude();//维度
+            double longitude = location.getLongitude();//经度
+            double[] doubles = CoordinateTransformUtil.wgs84togcj02(longitude,latitude);
+            latLongInfo = "维度：" + latitude + "\n精度:" + longitude+"\n维度：" + doubles[0] + "\n精度:" + doubles[1];
+        } else {
+            latLongInfo = "No location found";
+        }
+        mLabel.setText("Your current position is:\n" + latLongInfo);
+    }
+
+    private final LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            getLocationInfo(location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            getLocationInfo(null);
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            getLocationInfo(null);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        startSysLBS();
+    }
+
 }
---
+++ app/src/main/java/com/laker/lakerdemo/mvp/bean/User.java
@@ -0,0 +1,34 @@
+package com.laker.lakerdemo.mvp.bean;
+
+/**
+ * =============================================================================
+ * [YTF] (C)2015-2099 Yuantuan Inc.
+ * Link        http://www.ytframework.cn
+ * =============================================================================
+ *
+ * @author laker<lakerandroiddev@gmail.com>
+ * @created 2016/12/22
+ * @description description
+ * =============================================================================
+ */
+
+public class User {
+    private String name;
+    private String pwd;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+}
